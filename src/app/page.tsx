800b92d13828c104b81b664f15c7369342c34931
Aaron <offxcxal.aaron@icloud.com>
2025-04-10 00:09:40 -0700

    added title section debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-import { Mail, ExternalLink, Phone } from "lucide-react"
+import { Mail } from "lucide-react"
 
 import logo from "../../public/logo.png"
 import image01 from "../../public/image-01.webp"
